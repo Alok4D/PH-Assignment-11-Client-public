4a683cf78784ace1345c680f4aa595ecb4b27e0b
Alok4D <alokroy602701@gmail.com>
2024-05-15 19:37:40 +0600

    add search funtion

--- src/Pages-Component/All-Food-Card/AllCard.jsx
+++ src/Pages-Component/All-Food-Card/AllCard.jsx
@@ -1,34 +1,60 @@
-// import { useLoaderData } from "react-router-dom";
-// import ShowCard from "../ShowAllCard/ShowCard";
-// import { Helmet } from "react-helmet";
-
 import { useLoaderData } from "react-router-dom";
 import ShowCard from "../Show-Food-Card-All/ShowCard";
 import { Helmet } from "react-helmet";
-
+import { useEffect, useState } from "react";
 
 const AllCard = () => {
-    const allCard = useLoaderData();
-    console.log(allCard);
-    
-    return (
+  const allCard = useLoaderData();
+  console.log(allCard);
 
-        <div>
-           {/* <div className="w-[80%] mx-auto mt-10">
-           <select className="select select-bordered w-full max-w-xs">
-                <option>All Crafts</option>
-                <option>High Cost</option>
-                <option>Low Cost</option>
-            </select>
-           </div> */}
-        <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-5 lg:w-[80%] mx-auto  mt-10"  data-aos="zoom-in">
-            <Helmet><title>Our All Food Items !</title></Helmet>
-            {
-            allCard.map(allCard => <ShowCard allCard={allCard} key={allCard._id}></ShowCard>)
-            }
-        </div>
-        </div>
-    );
+  const [search, setSearch] = useState("");
+  const [foods, setFoods] = useState([]);
+
+  const handleFoodsSearch = (e) => {
+    e.preventDefault();
+    const searchText = e.target.search.value;
+    console.log(searchText);
+    setSearch(searchText);
+  };
+  
+  useEffect(() => {
+    fetch(`http://localhost:5000/foods?search=${search}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setFoods(data);
+      });
+  }, [search]);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Our All Food Items !</title>
+      </Helmet>
+      <div className="mt-10 mb-10 bg-slate-300 h-[100px] border rounded-xl">
+        <form onSubmit={handleFoodsSearch} className="text-center pt-4">
+          <input type="text" name="search" placeholder="search foods"
+            className="input input-bordered input-lg w-full max-w-xs mr-2" />
+          <input type="submit" value="Search" className="btn" />
+        </form>
+      </div>
+
+    
+     {
+        foods.length ?   <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-5 ">
+        {foods.map((food) => (
+          <ShowCard allCard={food} key={food._id}></ShowCard>
+        ))}
+      </div> :  <div
+        className="grid sm:grid-cols-1 lg:grid-cols-3 gap-5 lg:w-[80%] mx-auto  mt-10"
+        data-aos="zoom-in">
+        {allCard.map((allCard) => (
+          <ShowCard allCard={allCard} key={allCard._id}></ShowCard>
+        ))}
+      </div>
+     }
+    </div>
+  );
 };
 
 export default AllCard;
--- src/Pages-Component/Show-Food-Card-All/ShowCard.jsx
+++ src/Pages-Component/Show-Food-Card-All/ShowCard.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 
 const ShowCard = ({allCard}) => {
     console.log(allCard);
-    const  { photo,  price,   itemName, description} = allCard;
+    const  { photo,  price,  subcategory , quantity, foodName    } = allCard;
     return (
         <div className="max-w-[350px] space-y-4 rounded-lg bg-white p-6 shadow-lg md:w-[350px] dark:bg-[#18181B] mt-8 mb-8">
      <img src={photo} alt="Industrial-Products-Images" className="border rounded-xl h-[190px] " />
       <div className="grid gap-2">
-        <h1 className="text-lg font-semibold ">{itemName}</h1>
-        <p className="text-sm text-gray-500 dark:text-white/60 flex-grow">{description}</p>
-        <div className="text-lg font-semibold">${price}</div>
+        <h1 className="text-lg font-semibold ">Food Name : {foodName}</h1>
+        <p className="text-sm text-gray-500 dark:text-white/60 flex-grow">Category : #{subcategory}</p>
+       <div className="flex gap-10 ">
+       <div className="text-lg font-semibold">Price : ${price}</div>
+        <div className="text-lg font-semibold">Quantity : {quantity}</div>
+       </div>
       </div>
       <div className="flex gap-4">
-        <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">Add to Cart</button>
-      
        <Link to={`/cardDetails/${allCard._id}`}>
        <button className="rounded-md border border-black px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">View Details</button>
        </Link>
